c9424ae754ce9d996117a7d4c20423c80f1150d3
Richard Steadman <richard.steadman@gmail.com>
2020-11-23 14:48:00 -0500

    updates

---
+++ src/components/UserMenu.js
@@ -0,0 +1,42 @@
+//import { IProps, Menu, MenuDivider, MenuItem } from "@blueprintjs/core";
+import React, { Component } from "react";
+import { Menu, MenuDivider, MenuItem } from "@blueprintjs/core";
+
+//export interface IFileMenuProps extends IProps {
+//    shouldDismissPopover?: boolean;
+//}
+
+//export const UserMenu (props) => (
+
+export default class UserMenu extends Component {
+
+	render() {
+		return (
+			<Menu>
+				<MenuItem text="New" icon="document" />
+				<MenuItem text="Open" icon="folder-shared" />
+				<MenuItem text="Close" icon="add-to-folder" />
+				<MenuDivider />
+				<MenuItem text="Save" icon="floppy-disk" />
+				<MenuItem text="Save as..." icon="floppy-disk" />
+				<MenuDivider />
+				<MenuItem text="Exit" icon="cross" />
+			</Menu>
+		);
+	}
+}
+
+/*render() {
+	return (
+<Menu className={props.className}>
+	<MenuItem text="New" icon="document" {...props} />
+	<MenuItem text="Open" icon="folder-shared" {...props} />
+	<MenuItem text="Close" icon="add-to-folder" {...props} />
+	<MenuDivider />
+	<MenuItem text="Save" icon="floppy-disk" {...props} />
+	<MenuItem text="Save as..." icon="floppy-disk" {...props} />
+	<MenuDivider />
+	<MenuItem text="Exit" icon="cross" {...props} />
+</Menu>
+	);
+}*/
